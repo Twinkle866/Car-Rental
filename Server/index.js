063ba926fdff8dd4ed4df1ebe0a7152cb793b2c4
Twinkle Sharma <twinksharma18@gmail.com>
2025-09-14 23:20:38 +0530

    solved deploy issues

--- Server/index.js
+++ Server/index.js
@@ -1,36 +1,49 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
-
 import cors from "cors";
 import connectDB from "./Configs/db.js";
+
+
 import userRouter from "./routes/userRoutes.js";
 import ownerRouter from "./routes/ownerRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 
+dotenv.config();
+
 const app = express();
 
-// Middleware
-app.use(cors({
-  origin: [
-    "http://localhost:5173",          // local dev
-    "https://car-rental-client.onrender.com" // deployed frontend
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+connectDB();
+
 
 app.use(express.json());
 
-// Routes
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:5173", // local dev
+        "https://car-rental-client.onrender.com", // deployed frontend
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+app.get("/", (req, res) => res.send("Car Rental Backend is running "));
 app.use("/api/user", userRouter);
 app.use("/api/owner", ownerRouter);
 app.use("/api/bookings", bookingRouter);
 
-// Connect DB (only once)
-connectDB()
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-export default app
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+export default app;
